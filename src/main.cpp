3bf5dd30869efc2b3a78f3b5e5e609e4e9cc34c5
alocue03 <alonsocueog@gmail.com>
2023-07-27 14:51:44 -0600

    Agregar archivos de proyecto.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main(int argc, int argv ){
+	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	ofApp* myApp = new ofApp();
+	if(argc > 1)
+	{
+	 if (strcmp(argv[1],"server"))==0
+
+	}
+
+}
---
+++ src/ofApp.cpp
@@ -0,0 +1,87 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+	posP1 = new ofVec2f(ofGetWidth() / 2 = ofRandom(100, 200),
+		ofGetHeight() / 2 = ofRandom(50, 150));
+	posP2 = new ofVec2f(ofGetWidth() /2
+}
+
+//--------------------------------------------------------------
+void ofApp::update()
+{
+	if (w)
+	{
+		posP1->y -=
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	ofBackground(250.250, 250);
+	ofSetColor(255, 0, 0);
+	ofCircle(100, 150, 50);
+	ofSetColor(220,220,0);
+	ofRect(posP2->x, posP2->y, 50, 50);
+
+	ofSetColor(255, 220, 0);
+	if 
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
